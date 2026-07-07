cd57cde1bccb7debe356eede2bba9d847901083e
MusabGanatra <m.musab.ganatra@gmail.com>
2026-07-07 20:54:18 +0500

    Implement login function using Supabase client

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const supabaseUrl = "https://mfwcibpbeqqtgbyyught.supabase.co/rest/v1/";
+const supabaseKey = "sb_publishable_NvwPu7CHkFZkr45OyUO8oQ_FcY54C17";
 
+const supabaseClient = supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } =
+    await supabaseClient.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  alert("Login Successful");
+  window.location.href = "dashboard.html";
+}
